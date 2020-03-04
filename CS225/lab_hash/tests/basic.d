df2d4bb24945c4b3487fc2cab42d2a064ee0648e
ericpzh <zp3@illinois.edu>
2020-03-04 14:35:28 -0600

    Add files via upload

---
+++ CS225/lab_hash/tests/basic.d
@@ -0,0 +1,56 @@
+tests/basic.o: tests/basic.cpp tests/catch.hpp tests/catchlib.h \
+  tests/../textfile.h tests/../char_counter.h tests/../schashtable.h \
+  tests/../hashtable.h tests/../hashes.h tests/../htiterator.h \
+  tests/../htiterator.cpp tests/../sciterator.h tests/../sciterator.cpp \
+  tests/../schashtable.cpp tests/../lphashtable.h tests/../lpiterator.h \
+  tests/../lpiterator.cpp tests/../lphashtable.cpp \
+  tests/../char_counter.cpp tests/../word_counter.h \
+  tests/../word_counter.cpp tests/../anagram_finder.h \
+  tests/../anagram_finder.cpp tests/../logfile_parser.h \
+  tests/../logfile_parser.cpp
+
+tests/catch.hpp:
+
+tests/catchlib.h:
+
+tests/../textfile.h:
+
+tests/../char_counter.h:
+
+tests/../schashtable.h:
+
+tests/../hashtable.h:
+
+tests/../hashes.h:
+
+tests/../htiterator.h:
+
+tests/../htiterator.cpp:
+
+tests/../sciterator.h:
+
+tests/../sciterator.cpp:
+
+tests/../schashtable.cpp:
+
+tests/../lphashtable.h:
+
+tests/../lpiterator.h:
+
+tests/../lpiterator.cpp:
+
+tests/../lphashtable.cpp:
+
+tests/../char_counter.cpp:
+
+tests/../word_counter.h:
+
+tests/../word_counter.cpp:
+
+tests/../anagram_finder.h:
+
+tests/../anagram_finder.cpp:
+
+tests/../logfile_parser.h:
+
+tests/../logfile_parser.cpp:
---
+++ CS225/lab_hash/word_counter.cpp
@@ -0,0 +1,44 @@
+/**
+ * @file word_counter.cpp
+ * Implementation of the WordFreq class.
+ *
+ * @author Chase Geigle
+ * @date Spring 2011
+ * @date Spring 2012
+ */
+
+using std::vector;
+using std::pair;
+using std::string;
+using std::ifstream;
+using std::istringstream;
+using std::cout;
+using std::endl;
+
+template <template <class K, class V> class Dict>
+WordFreq<Dict>::WordFreq(const string& infile)
+    : filename(infile)
+{
+    /* nothing */
+}
+
+template <template <class K, class V> class Dict>
+vector<pair<string, int>> WordFreq<Dict>::getWords(int threshold) const
+{
+    TextFile infile(filename);
+    vector<pair<string, int>> ret;
+    /**
+     * @todo Implement this function.
+     * @see char_counter.cpp if you're having trouble.
+     */
+     Dict<string, int> table(9999);
+     while(infile.good()){
+       string str = infile.getNextWord();
+       table[str]++;
+     }
+     for (auto & i : table) {
+       if (i.second >= threshold)
+        ret.push_back(i);
+      }
+    return ret;
+}
